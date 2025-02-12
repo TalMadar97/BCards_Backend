8ff6ba95cd5cd8429016d909ef27c00281c1232e
TalMadar97 <talmadar360@gmail.com>
2025-02-12 11:14:12 +0200

    Create my cards

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -8,3 +8,15 @@ exports.getAllCards = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+exports.getUserCards = async (req, res) => {
+  try {
+    const userId = req.user.id; // Get logged-in user ID
+    const userCards = await Card.find({ user_id: userId });
+
+    res.status(200).json(userCards);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
--- routes/cardRoutes.js
+++ routes/cardRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCards } = require("../controllers/cardController");
+const { getAllCards, getUserCards } = require("../controllers/cardController");
+const { authenticate } = require("../middlewares/authMiddleware");
 
 // Get all cards (Public)
 router.get("/", getAllCards);
+
+// Get user's own cards (Authenticated users)
+router.get("/my-cards", authenticate, getUserCards);
 
 module.exports = router;
